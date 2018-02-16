964abd9de40213af93334538220541d395b4486c
Simon Hoyos <shmesa23@gmail.com>
2018-02-16 08:10:44 -0500

    Add board model tests

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -12,7 +12,8 @@
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Shouldn't create board without title" do
+    board = Board.new()
+    assert_not board.save
+  end
 end
